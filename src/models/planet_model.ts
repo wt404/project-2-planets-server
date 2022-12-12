6030c5f7638570967a4600cae5c6292334375243
jasonlerit <jasonlerit077@gmail.com>
2022-12-13 00:06:35 +0800

    added planet model

---
+++ src/models/planet_model.ts
@@ -0,0 +1,29 @@
+
+import { Schema, model } from "mongoose"
+
+interface PlanetImageInterface {
+    description: string
+    url: string
+}
+
+interface PlanetInterface {
+    name: string
+    description: string
+    planetOrder: number
+    mass: string
+    volume: string
+    images: PlanetImageInterface[]
+}
+
+const schema = new Schema<PlanetInterface>({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    planetOrder: { type: Number, required: true },
+    mass: { type: String, required: true },
+    volume: { type: String, required: true },
+    images: { type: [], required: true }
+})
+
+const PlanetModel = model<PlanetInterface>('Planet', schema)
+
+export default PlanetModel
