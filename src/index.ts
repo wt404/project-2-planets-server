e93ece7c2c708741c89d4033881d67b1e0e06c06
jasonlerit <jasonlerit077@gmail.com>
2022-12-12 23:21:55 +0800

    added express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 
-console.log('Test')
+import express, { Application } from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+const app: Application = express()
+
+app.use(bodyParser.json({ limit: '30mb' }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json('Test')
+})
+
+app.listen((process.env.PORT || 5000), () => console.log(`Server running`))
