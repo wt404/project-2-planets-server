21563048c239249dad5871dc8d6b730757cf7e21
jasonlerit <jasonlerit077@gmail.com>
2022-12-13 00:34:36 +0800

    added sort

--- src/controllers/planet_controller.ts
+++ src/controllers/planet_controller.ts
@@ -4,7 +4,7 @@ import PlanetModel from "../models/planet_model"
 
 export const getPlanets = async (req: Request, res: Response) => {
     try {
-        const planets = await PlanetModel.find()
+        const planets = await PlanetModel.find().sort({ _id: 1, })
         res.json(planets)
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong' })
