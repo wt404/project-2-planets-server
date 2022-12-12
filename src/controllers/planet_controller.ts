dfd858d3cf63a0a8ad1ea5a6a4b1269bba264b85
jasonlerit <jasonlerit077@gmail.com>
2022-12-13 00:21:30 +0800

    updated planet controller

--- src/controllers/planet_controller.ts
+++ src/controllers/planet_controller.ts
@@ -1,6 +1,12 @@
 
 import { Request, Response } from "express"
+import PlanetModel from "../models/planet_model"
 
 export const getPlanets = async (req: Request, res: Response) => {
-    res.json("Planets")
+    try {
+        const planets = await PlanetModel.find()
+        res.json(planets)
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong' })
+    }
 }
