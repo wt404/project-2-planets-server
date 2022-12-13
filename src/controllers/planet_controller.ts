20f4b63250caf9ff76dc823d971f5811eaaede10
jasonlerit <jasonlerit077@gmail.com>
2022-12-13 21:11:23 +0800

    added get planet

--- src/controllers/planet_controller.ts
+++ src/controllers/planet_controller.ts
@@ -1,4 +1,5 @@
 
+import mongoose from 'mongoose'
 import { Request, Response } from "express"
 import PlanetModel from "../models/planet_model"
 
@@ -7,6 +8,18 @@ export const getPlanets = async (req: Request, res: Response) => {
         const planets = await PlanetModel.find().sort({ _id: 1, })
         res.json(planets)
     } catch (error) {
+        res.status(500).json({ message: 'Something went wrong' })
+    }
+}
+
+export const getPlanet = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ message: 'Invalid id' })
+        const planet = await PlanetModel.find({ _id: id })
+        res.json(planet)
+    } catch (error) {
+        console.log(error)
         res.status(500).json({ message: 'Something went wrong' })
     }
 }
--- src/routes/planet_route.ts
+++ src/routes/planet_route.ts
@@ -1,9 +1,10 @@
 
 import express from 'express'
-import { getPlanets } from '../controllers/planet_controller'
+import { getPlanets, getPlanet } from '../controllers/planet_controller'
 
 const router = express.Router()
 
 router.get('/', getPlanets)
+router.get('/:id', getPlanet)
 
 export default router
