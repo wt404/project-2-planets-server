eba5e309ec8e7428f9583c4b3b2a4afc575d39f2
jasonlerit <jasonlerit077@gmail.com>
2022-12-21 23:08:25 +0800

    updated register function

--- src/controllers/auth_controller.ts
+++ src/controllers/auth_controller.ts
@@ -2,7 +2,10 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { Request, Response } from "express"
+import Mailjet from "node-mailjet"
 import UserModel from "../models/user_mode"
+import VerificationModel from '../models/verification_model'
+import { generateToken } from './verification_controller'
 
 export const register = async (req: Request, res: Response) => {
     try {
@@ -31,12 +34,46 @@ export const register = async (req: Request, res: Response) => {
         const existingUser = await UserModel.findOne({ email })
         if (existingUser) return res.status(400).json({ message: 'Email not available' })
         const hashedPassword = await bcrypt.hash(password, 12)
-        UserModel.create({
+        const user = await UserModel.create({
             firstName: first_name,
             lastName: last_name,
             email,
             password: hashedPassword
         })
+
+        const verification = await VerificationModel.create({
+            user_id: user._id,
+            type: 'account',
+            token: await generateToken(),
+        })
+
+        const link = `https://space-verse.vercel.app/verify/${user._id}/${verification.token}`
+
+        const mailjet = Mailjet.apiConnect(
+            process.env.MJ_APIKEY_PUBLIC!,
+            process.env.MJ_APIKEY_PRIVATE!,
+        )
+
+        mailjet.post('send', { version: 'v3.1' }).request({
+            Messages: [
+                {
+                    From: {
+                        Email: "spaceversegroup2@gmail.com",
+                        Name: "SpaceVerse"
+                    },
+                    To: [
+                        {
+                            Email: email,
+                            Name: `${first_name} ${last_name}`
+                        },
+                    ],
+                    Subject: "Verify your Account",
+                    TextPart: `Hello ${first_name}, please verify your account by clicking this link: ${link}`,
+                    HTMLPart: ""
+                }
+            ]
+        })
+
         res.json('Successfully registered')
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong' })
