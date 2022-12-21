a68a388badf72eaf97aa3588b086a3e2423d08e5
jasonlerit <jasonlerit077@gmail.com>
2022-12-22 07:48:55 +0800

    updated forgot password route and controller

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -2,11 +2,13 @@
 
 import express from 'express'
 import { register, login, loginWithGoogle } from '../controllers/auth_controller'
+import { forgotPassword } from '../controllers/auth/forgot_password_controller'
 
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
 router.post('/google', loginWithGoogle)
+router.post('/forgot-password', forgotPassword)
 
 export default router
