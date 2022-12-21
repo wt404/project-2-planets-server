1d394ee502f59698318312f27a4aa47a811e38ea
jasonlerit <jasonlerit077@gmail.com>
2022-12-21 19:45:58 +0800

    updated register function

--- src/controllers/auth_controller.ts
+++ src/controllers/auth_controller.ts
@@ -1,9 +1,21 @@
 
+import bcrypt from 'bcrypt'
 import { Request, Response } from "express"
+import UserModel from "../models/user_mode"
 
 export const register = async (req: Request, res: Response) => {
     try {
-        res.json("Register")
+        const { first_name, last_name, email, password } = req.body
+        const existingUser = await UserModel.findOne({ email })
+        if (existingUser) return res.status(400).json({ message: 'Email not available' })
+        const hashedPassword = await bcrypt.hash(password, 12)
+        UserModel.create({
+            firstName: first_name,
+            lastName: last_name,
+            email,
+            password: hashedPassword
+        })
+        res.json('Successfully registered')
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong' })
     }
