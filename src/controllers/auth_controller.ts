89bdc9346b7ae9d5597b751cc1560b3c47d90d0c
jasonlerit <jasonlerit077@gmail.com>
2022-12-21 22:04:10 +0800

    added validation

--- src/controllers/auth_controller.ts
+++ src/controllers/auth_controller.ts
@@ -7,6 +7,27 @@ import UserModel from "../models/user_mode"
 export const register = async (req: Request, res: Response) => {
     try {
         const { first_name, last_name, email, password } = req.body
+
+        if (first_name == undefined || first_name == '') return res.status(400).json({
+            message: 'Invalid first name',
+            type: 'first_name'
+        })
+
+        if (last_name == undefined || last_name == '') return res.status(400).json({
+            message: 'Invalid last name',
+            type: 'last_name'
+        })
+
+        if (email == undefined || email == '') return res.status(400).json({
+            message: 'Invalid email',
+            type: 'email'
+        })
+
+        if (password == undefined || password == '') return res.status(400).json({
+            message: 'Invalid password',
+            type: 'password'
+        })
+
         const existingUser = await UserModel.findOne({ email })
         if (existingUser) return res.status(400).json({ message: 'Email not available' })
         const hashedPassword = await bcrypt.hash(password, 12)
@@ -25,6 +46,17 @@ export const register = async (req: Request, res: Response) => {
 export const login = async (req: Request, res: Response) => {
     try {
         const { email, password } = req.body
+
+        if (email == undefined || email == '') return res.status(400).json({
+            message: 'Invalid email',
+            type: 'email'
+        })
+
+        if (password == undefined || password == '') return res.status(400).json({
+            message: 'Invalid password',
+            type: 'password'
+        })
+
         const existingUser = await UserModel.findOne({ email })
         if (!existingUser) return res.status(400).json({ message: 'Invalid credentials' })
         const isPasswordMatch = await bcrypt.compare(password, existingUser.password)
