8e321d2e0c9ae50c001a732c9833ccc6bd924aa5
jasonlerit <jasonlerit077@gmail.com>
2022-12-21 22:43:52 +0800

    update verification model

--- src/models/verification_model.ts
+++ src/models/verification_model.ts
@@ -4,13 +4,13 @@ import { Schema, model, ObjectId } from "mongoose";
 interface VerificationInterface {
     user_id: ObjectId
     type: string
-    key: string
+    token: string
 }
 
 const schema = new Schema<VerificationInterface>({
     user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     type: { type: String, required: true },
-    key: { type: String, required: true }
+    token: { type: String, required: true }
 })
 
 const VerificationModel = model<VerificationInterface>('Verification', schema)
