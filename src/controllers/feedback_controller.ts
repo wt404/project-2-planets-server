d59dca0df3ff0ae793e2e628c668df154ee52db4
jasonlerit <jasonlerit077@gmail.com>
2022-12-21 15:31:39 +0800

    updated feedback controller

--- src/controllers/feedback_controller.ts
+++ src/controllers/feedback_controller.ts
@@ -1,6 +1,81 @@
 
 import { Request, Response } from "express"
+import Mailjet from "node-mailjet"
 
 export const sendFeedback = async (req: Request, res: Response) => {
-    res.json("Done")
+    try {
+        
+        const body = req.body
+
+        if (body.first_name == '') return res.status(400).json({
+            message: 'Invalid first name',
+            type: 'first_name'
+        })
+
+        if (body.last_name == '') return res.status(400).json({
+            message: 'Invalid last name',
+            type: 'last_name'
+        })
+
+        if (body.email == '') return res.status(400).json({
+            message: 'Invalid email',
+            type: 'email'
+        })
+
+        if (body.message == '') return res.status(400).json({
+            message: 'Invalid message',
+            type: 'message'
+        })
+
+        const mailjet = Mailjet.apiConnect(
+            process.env.MJ_APIKEY_PUBLIC!,
+            process.env.MJ_APIKEY_PRIVATE!,
+        )
+
+        const request = mailjet
+            .post('send', { version: 'v3.1' })
+            .request({
+                Messages: [
+                    {
+                        From: {
+                            Email: "spaceversegroup2@gmail.com",
+                            Name: "SpaceVerse Feedback"
+                        },
+                        To: [
+                            {
+                                Email: "alandemol05@gmail.com",
+                                Name: "Alan Demol"
+                            },
+                            {
+                                Email: "kap.utolclothing@gmail.com",
+                                Name: "Angela Morales"
+                            },
+                            {
+                                Email: "jasonlerit077@gmail.com",
+                                Name: "Jason Lerit"
+                            },
+                            {
+                                Email: "cladojomar881@gmail.com",
+                                Name: "Jomar Clado"
+                            }
+                        ],
+                        Subject: "Feedback Received",
+                        TextPart: `First Name: ${body.first_name}\nLast Name: ${body.last_name}\nEmail: ${body.email}\nMessage: ${body.message}`,
+                        HTMLPart: ""
+                    }
+                ]
+            })
+
+        request
+            .then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
+
+        res.json("Feedback sent")
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong' })
+    }
 }
