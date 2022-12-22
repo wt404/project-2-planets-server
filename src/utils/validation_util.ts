f1ac85a5441ab4ea30f70214e257b3c42c6d3f1e
jasonlerit <jasonlerit077@gmail.com>
2022-12-22 17:35:52 +0800

    added validation util

---
+++ src/utils/validation_util.ts
@@ -0,0 +1,40 @@
+
+export const validateFirstName = (value: string) => {
+    if (value.length === 0) {
+        return 'Please enter your first name'
+    }
+    if (value.length > 30) {
+        return 'Max 30 characters only'
+    }
+    if (!/^[A-Za-z0-9 ]*$/.test(value)) {
+        return 'Only alpha num and space are allowed'
+    }
+    return null
+}
+
+export const validateLastName = (value: string) => {
+    if (value.length === 0) {
+        return 'Please enter your last name'
+    }
+    if (value.length > 30) {
+        return 'Max 30 characters only'
+    }
+    if (!/^[A-Za-z0-9 ]*$/.test(value)) {
+        return 'Only alpha num and space are allowed'
+    }
+    return null
+}
+
+export const validateEmail = (value: string) => {
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+        return 'Invalid email'
+    }
+    return null
+}
+
+export const validatePassword = (value: string) => {
+    if (value.length < 8) {
+        return 'Minimum is 8'
+    }
+    return null
+}
