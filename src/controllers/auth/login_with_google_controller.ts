a8b60107a2c6e584c0c1b31c558ebbfe4a759c66
jasonlerit <jasonlerit077@gmail.com>
2022-12-22 11:21:09 +0800

    updated login with google controller

--- src/controllers/auth/login_with_google_controller.ts
+++ src/controllers/auth/login_with_google_controller.ts
@@ -1,10 +1,48 @@
 
+import jwt from 'jsonwebtoken'
 import { Request, Response } from "express"
+import { OAuth2Client } from 'google-auth-library'
+import UserModel from "../../models/user_model"
+const client = new OAuth2Client(process.env.GOOGLE_OAUTH_CLIENT_ID)
 
 export const loginWithGoogle = async (req: Request, res: Response) => {
     try {
-        res.json("Login with google")
+        const { clientId, credential } = req.body
+
+        const loginTicket = await client.verifyIdToken({
+            idToken: credential,
+            audience: process.env.GOOGLE_OAUTH_CLIENT_ID
+        })
+
+        const payload = loginTicket.getPayload()
+
+        if (payload == undefined) return res.status(400).json({ message: 'Invalid credentials' })
+
+        let existingUser = await UserModel.findOne({ email: payload.email })
+
+        if (!existingUser) {
+            existingUser = await UserModel.create({
+                firstName: payload.given_name,
+                lastName: payload.family_name,
+                email: payload.email,
+                verified: true,
+            })
+        }
+        
+        const token = jwt.sign(
+            {
+                first_name: existingUser.firstName,
+                last_name: existingUser.lastName,
+                email: existingUser.email,
+            },
+            process.env.JTW_SECRET!,
+            {
+                expiresIn: process.env.JWT_EXPIRES_IN
+            }
+        )
+        res.json({ token })
     } catch (error) {
+        console.log(error)
         res.status(500).json({ message: 'Something went wrong' })
     }
 }
