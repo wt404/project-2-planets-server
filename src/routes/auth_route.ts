fd87b220512e116b912921a686480073fa7622c5
jasonlerit <jasonlerit077@gmail.com>
2022-12-22 08:02:57 +0800

    changed forgot password to reset password

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -4,13 +4,13 @@ import express from 'express'
 import { register } from '../controllers/auth/register_controller'
 import { login } from '../controllers/auth/login_controller'
 import { loginWithGoogle } from '../controllers/auth/login_with_google_controller'
-import { forgotPassword } from '../controllers/auth/forgot_password_controller'
+import { resetPassword } from '../controllers/auth/reset_password_controller'
 
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
 router.post('/google', loginWithGoogle)
-router.post('/forgot-password', forgotPassword)
+router.post('/reset-password', resetPassword)
 
 export default router
