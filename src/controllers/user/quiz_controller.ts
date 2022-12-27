ec0f49c41115ccab41951b3df021588e62f936b2
jasonlerit <jasonlerit077@gmail.com>
2022-12-28 00:03:41 +0800

    added submit answer

--- src/controllers/user/quiz_controller.ts
+++ src/controllers/user/quiz_controller.ts
@@ -25,7 +25,6 @@ export const getQuiz = async (req: Request, res: Response) => {
 
             return res.json({
                 status: 'answering',
-                questionId: quizData._id,
                 question: quizData.question,
                 choices: quizData.choices,
                 totalCompletedQuestion: leaderboard.completedQuestionList.length
@@ -68,11 +67,91 @@ export const startQuiz = async (req: Request, res: Response) => {
 
         res.json({
             status: 'answering',
-            questionId: quizData._id,
             question: quizData.question,
             choices: quizData.choices,
             totalCompletedQuestion: leaderboard.completedQuestionList.length
         })
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong' })
+    }
+}
+
+export const submitAnswer = async (req: Request, res: Response) => {
+    try {
+        const { answer } = req.body
+
+        const existingUser = await UserModel.findOne({ email: req.user.email })
+        if (!existingUser) return res.status(400).json({ message: 'Invalid credentials' })
+
+        const leaderboard = await LeaderboardModel.findOne({ user_id: existingUser._id })
+
+        /* Not started yet */
+        if (!leaderboard) return res.status(400).json({ message: 'Start quiz' })
+
+        /* Already done */
+        if (leaderboard.finishedAt !== null) return res.status(400).json({ message: 'Already finished' })
+
+        /* Check if does not have enough time */
+        const startedAt = new Date(leaderboard.startedAt).getTime()
+        const currentTime = new Date().getTime()
+        const totalSeconds = (currentTime - startedAt) / 1000
+        if (totalSeconds >= 3600) {
+            const finishedAt = new Date()
+            await LeaderboardModel.findByIdAndUpdate(leaderboard._id, { finishedAt })
+            return res.json({
+                status: 'done',
+                score: leaderboard.score,
+                startedAt: leaderboard.startedAt,
+                finishedAt: finishedAt
+            })
+        }
+
+        /* Check answer if correct */
+        const quizData = await QuizModel.findOne({ _id: leaderboard.questionId })
+        if (!quizData) return res.status(400).json({ message: 'Question not found' })
+        if (answer == quizData.answer) {
+            await LeaderboardModel.findByIdAndUpdate(leaderboard._id, { score: leaderboard.score + 1 })
+        }
+
+        /* Add current questionId to completedQuestionList */
+        let completedQuestionList = leaderboard.completedQuestionList
+        completedQuestionList.push(quizData._id)
+        await LeaderboardModel.findByIdAndUpdate(leaderboard._id, { completedQuestionList })
+
+        /* Check if completed */
+        if (completedQuestionList.length >= 5) {
+            const finishedAt = new Date()
+            await LeaderboardModel.findByIdAndUpdate(leaderboard._id, { finishedAt })
+            return res.json({
+                status: 'done',
+                score: leaderboard.score,
+                startedAt: leaderboard.startedAt,
+                finishedAt: finishedAt
+            })
+        }
+
+        /* Get random question */
+        const randomQuestion = await QuizModel.aggregate([
+            {
+                $match: { _id: { $nin: completedQuestionList }}
+            },
+            {
+                $sample: { size: 1 }
+            }
+        ])
+
+        if (randomQuestion.length == 0) return res.status(400).json({ message: 'No more questions' })
+
+        /* Update questionId */
+        await LeaderboardModel.findByIdAndUpdate(leaderboard._id, { questionId: randomQuestion[0]._id })
+        
+        res.json({
+            status: 'answering',
+            question: randomQuestion[0].question,
+            choices: randomQuestion[0].choices,
+            totalCompletedQuestion: completedQuestionList.length
+        })
+
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong' })
     }
--- src/routes/user/quiz_route.ts
+++ src/routes/user/quiz_route.ts
@@ -1,10 +1,11 @@
 
 import express from 'express'
-import { getQuiz, startQuiz } from '../../controllers/user/quiz_controller'
+import { getQuiz, startQuiz, submitAnswer } from '../../controllers/user/quiz_controller'
 
 const router = express.Router()
 
 router.get('/', getQuiz)
 router.post('/', startQuiz)
+router.post('/submit', submitAnswer)
 
 export default router
