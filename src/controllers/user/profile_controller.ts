0b1588e61a5b87f39791714e33894acd6d8d408f
jasonlerit <jasonlerit077@gmail.com>
2022-12-28 10:09:43 +0800

    updated profile controller

--- src/controllers/user/profile_controller.ts
+++ src/controllers/user/profile_controller.ts
@@ -1,9 +1,17 @@
 
 import { Request, Response } from "express"
+import UserModel from "../../models/user_model"
 
 export const getProfile = async (req: Request, res: Response) => {
     try {
-        res.json("Profile")
+        const existingUser = await UserModel.findOne({ email: req.user.email })
+        if (!existingUser) return res.status(400).json({ message: 'Invalid credentials' })
+        res.json({
+            avatar: existingUser.avatar,
+            firstName: existingUser.firstName,
+            lastName: existingUser.lastName,
+            email: existingUser.email
+        })
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong' })
     }
