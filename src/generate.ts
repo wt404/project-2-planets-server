8917e33ab58d10648d554c4c3abc61d97fc4b37c
jasonlerit <jasonlerit077@gmail.com>
2022-12-29 19:22:48 +0800

    added quiz data

--- src/generate.ts
+++ src/generate.ts
@@ -6,9 +6,11 @@ import mongoose from 'mongoose'
 
 import planetsJson from './json/planets.json'
 import newsJson from './json/news.json'
+import quizJson from './json/quiz.json'
 
 import PlanetModel from './models/planet_model'
 import NewsModel from './models/news_model'
+import QuizModel from './models/quiz_model'
 
 mongoose.set('strictQuery', true)
 
@@ -21,6 +23,9 @@ mongoose.connect(process.env.CONNECTION_URL!).then(async () => {
 
     await NewsModel.deleteMany()
     await NewsModel.create(newsJson)
+
+    await QuizModel.deleteMany()
+    await QuizModel.create(quizJson)
 
     console.log('Done')
 
