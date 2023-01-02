1fac3e0833cf67b844014548757ae82de8290d22
jasonlerit <jasonlerit077@gmail.com>
2023-01-02 22:15:14 +0800

    updated profile controller

--- src/controllers/user/profile_controller.ts
+++ src/controllers/user/profile_controller.ts
@@ -20,12 +20,12 @@ export const getProfile = async (req: Request, res: Response) => {
 export const updateProfile = async (req: Request, res: Response) => {
     try {
 
-        const { avatar, first_name, last_name } = req.body
+        const { avatar, firstName, lastName } = req.body
 
         const existingUser = await UserModel.findOne({ email: req.user.email })
         if (!existingUser) return res.status(400).json({ message: 'Invalid credentials' })
 
-        await UserModel.findByIdAndUpdate(existingUser._id, { avatar, firstName: first_name, lastName: last_name })
+        await UserModel.findByIdAndUpdate(existingUser._id, { avatar, firstName, lastName })
 
         res.json({ message: 'Profile updated.' })
     } catch (error) {
